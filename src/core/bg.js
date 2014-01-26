8e71bbe226244a814a98efd93d171edc49b871d9
Sergey Bevzuk <smokeelow@gmail.com>
2014-01-26 21:37:31 +0200

    improved screenshot capture logic, less memory usage

--- src/core/bg.js
+++ src/core/bg.js
@@ -1,11 +1,18 @@
 /**
- * Track all tabs updates
+ * Calculate how many days passed
  *
- * @param tabID
- * @param tabState
- * @param tab
+ * @param firstDate timestamp
+ * @param secondDate timestamp
+ * @returns {number}
  */
+Date.daysPassed = function(firstDate, secondDate) {
+    var day = 1000 * 60 * 60 * 24,
+        diff = secondDate - firstDate;
 
+    return Math.round(diff / day);
+};
+
+//Check For File System Object
 window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 
 /**
@@ -14,62 +21,103 @@ window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileS
  */
 var fs = null;
 
+/**
+ * File System Errors
+ *
+ * @param e
+ */
 function errorHandler(e) {
     console.log('Error: ' + e.name);
 }
 
+/**
+ * Set File System Var
+ */
 function fsInit() {
     window.requestFileSystem(window.TEMPORARY, 10 * 1024 * 1024, function(filesystem) {
         fs = filesystem;
-
-        fs.root.getFile('vk_com.jpeg', {}, function(filentry){
-            filentry.getMetadata(function(meta){
-
-            })
-        });
     }, errorHandler);
 }
 
+//Load File system
 fsInit();
 
+/**
+ * Track all tabs updates
+ *
+ * @param tabID
+ * @param tabState
+ * @param tab
+ */
 chrome.tabs.onUpdated.addListener(function(tabID, tabState, tab) {
-
-    //wait for full load
     if(tab.url !== undefined && tabState.status == 'complete') {
 
-        if(localStorage['dashSites'].indexOf(tab.url.split('/')[2].replace('www.', '')) > -1) {
-            chrome.tabs.captureVisibleTab(null, function(dataURI) {
+        var cacheTabId = tab.id,
+            domain = tab.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
 
-                var data = atob(dataURI.substring('data:image/jpeg;base64,'.length)),
-                    uArr = new Uint8Array(data.length),
-                    imgName = tab.url.split('/')[2].replace('www.', '').replace('.', '_') + '.jpeg';
+        //if url is ok
+        if(domain)
+            domain = domain[1].replace('www.', '');
 
-                for(var i = 0, size = data.length; i < size; ++i)
-                    uArr[i] = data.charCodeAt(i);
+        if(localStorage['dashSites'].indexOf(domain) > -1) {
 
-                fs.root.getFile(imgName, {create: true}, function(fileEntry) {
+            var imgName = domain.replace('.', '_') + '.jpeg';
 
-                    fileEntry.createWriter(function(fileWriter) {
+            //check if file exists
+            fs.root.getFile(imgName, {create: true}, function(filentry) {
 
-                        fileWriter.onwriteend = function(e) {
+                filentry.getMetadata(function(meta) {
 
-                            var jsonArr = JSON.parse(localStorage['dashSites']);
+                    //check if file older than one day
+                    if(Date.daysPassed(new Date(meta.modificationTime).getTime(), new Date().getTime()) >= 1) {
 
-                            for(var i = 0, size = jsonArr.length; i < size; i++) {
-                                if(jsonArr[i].url.indexOf(tab.url.split('/')[2].replace('www.', '')) > -1)
-                                    jsonArr[i].image = fileEntry.toURL();
-                            }
+                        //capture screenshot of current page
+                        chrome.tabs.captureVisibleTab(null, function(dataURI) {
 
-                            localStorage['dashSites'] = JSON.stringify(jsonArr);
-                        };
+                            //get current active tab
+                            chrome.tabs.getSelected(function(tab) {
 
-                        fileWriter.onerror = function(e) {
-                            console.log('Write failed: ' + e.toString());
-                        };
+                                //compare with opened/updated tab
+                                if(cacheTabId == tab.id) {
+                                    //convert to bytes
+                                    var data = atob(dataURI.substring('data:image/jpeg;base64,'.length)),
+                                        uArr = new Uint8Array(data.length);
 
-                        fileWriter.write(new Blob([ uArr.buffer ], {type: 'image/jpeg'}));
+                                    for(var i = 0, size = data.length; i < size; ++i)
+                                        uArr[i] = data.charCodeAt(i);
 
-                    }, errorHandler);
+                                    //save file to File System and LocalStorage
+                                    fs.root.getFile(imgName, {create: true}, function(fileEntry) {
+
+                                        fileEntry.createWriter(function(fileWriter) {
+
+                                            fileWriter.onwriteend = function(e) {
+
+                                                var jsonArr = JSON.parse(localStorage['dashSites']);
+
+                                                for(var i = 0, size = jsonArr.length; i < size; i++) {
+
+                                                    if(tab.url == jsonArr[i].url)
+                                                        jsonArr[i].image = fileEntry.toURL();
+                                                    else if(jsonArr[i].url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)[1].replace('www.', '') == domain)
+                                                        jsonArr[i].image = fileEntry.toURL();
+                                                }
+
+                                                localStorage['dashSites'] = JSON.stringify(jsonArr);
+                                            };
+
+                                            fileWriter.onerror = function(e) {
+                                                console.log('Write failed: ' + e.toString());
+                                            };
+
+                                            fileWriter.write(new Blob([ uArr.buffer ], {type: 'image/jpeg'}));
+
+                                        }, errorHandler);
+                                    });
+                                }
+                            });
+                        });
+                    }
                 });
             });
         }
